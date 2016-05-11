fbe6d1901b6e8499d1360ccc5904cd62d2be8f4a
onitrebla <tino477@gmail.com>
2016-05-11 11:34:39 -0300

    site desativado temporariamente

---
+++ indexe.php
@@ -0,0 +1,462 @@
+<?php include_once 'parts/header.php'; ?>
+
+
+    <body style="overflow-x: hidden;" class="body">
+        <!-- Top content -->
+        <div class="top-content section-container-5">
+            <!-- Top menu -->
+            <nav class="navbar navbar-inverse na vbar-no-bg" role="navigation" style=" width: 100%; background-color: #fff; top: 40px">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button class="navbar-toggle collapsed" data-target="#top-navbar-1" data-toggle="collapse" type="button">
+                            <span class="sr-only">
+                                Toggle navigation
+                            </span>
+                            <span class="icon-bar">
+                            </span>
+                            <span class="icon-bar">
+                            </span>
+                            <span class="icon-bar">
+                            </span>
+                        </button>
+                    </div>
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse cor-menu" id="top-navbar-1">
+                        <div class="row">
+
+
+                            <div class="col-md-5">
+                                <ul class="nav navbar-nav navbar-left">
+                                <li>
+                                        <a class="scroll-link" href="#testimonials">
+                                          <i class="fa fa-shopping-bag"></i>  Loja
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="scroll-link" href="#divirta-se">
+                                            Divirta-se
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="scroll-link" href="#about-us">
+                                            Trio Espuleta
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+
+
+                            <div class="col-md-2">
+                                <img alt="" src="img/logo.png" class="logotipo" style="border:none;">
+                            </div>
+
+
+                            <div class="col-md-5">
+                                <ul class="nav navbar-nav navbar-right">
+                                    <li>
+                                        <a class="scroll-link" href="#app-features">
+                                            O Projeto
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="scroll-link" href="#how-it-works">
+                                          A Idealizadora
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a class="scroll-link" href="#blog">
+                                         Blog
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="scroll-link" href="#contato">
+                                            Contato
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
+<div class="row" style="position: relative; top: 20px; width: auto">
+        <!-- Carousel -->
+        <div id="carousel-example-generic" class="carousel slide" data-ride=""> <!-- carousel  atributo da dataride-->
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+                <div class="item active">
+                    <img class="slideClass" src="img/slide-1.png" alt="First slide">
+                    <!-- Static Header -->
+                </div>
+                <div class="item">
+                    <img class="slideClass" src="img/slide-1.png" alt="Second slide">
+                    <!-- Static Header -->
+                </div>
+
+            </div>
+        </div><!-- /carousel -->
+
+                <div>
+                        <a class="scroll-link" href="#app-features">
+                         <img src="img/seta.png" style="position: relative;top: 14px; z-index: 999;">
+                        </a>
+                </div>
+                <a href="#" class="scrollToTop">
+                    <!-- <i class="fa fa-arrow-circle-o-up fa-4x bt" title="Subir ao Topo"></i> -->
+                    <img src="img/seta.png" class="img_180 bt" title="Subir ao Topo" />
+                </a>
+
+</div>
+
+        </div>
+
+
+
+
+    </body>
+</html>
+
+<!-- Always beautiful -->
+<div class="testimonials-container section-container-3 letras-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 always-beautiful section-description wow fadeIn">
+                <h2 class="letras-footer">
+                    Loja
+                </h2>
+                <div class="col-md-8">
+                    <img alt="" src="img/livro-1.jpg">
+                </div>
+                <div class="col-md-4">
+                    <div align="left">
+                        <span> Série As Aventura do Trio Espuleta</span> <br>
+                        <span style="font-size: 20px; font-weight: bold;">EXPLORANDO O CORAÇÃO</span><br>
+                        <a href="loja.php" class="btn btn-group btn-info"> Comprar Agora</a>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <img alt="" src="img/livro-2.jpg">
+                </div>
+                <div class="col-md-4">
+                    <div align="left">
+                        <span> Série As Aventura do Trio Espuleta</span> <br>
+                        <span style="font-size: 20px; font-weight: bold;">De Repente um Túnel</span><br>
+                        <a href="" class="btn btn-group btn-danger" style="pointer-events: none;"> Não disponível</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <img src="img/aviao.png" style="position: relative; top: 100px; left: 25%">
+    
+    </div>
+    
+</div>
+
+
+<div class="divirta-se-container section-container">
+    <div class="container">
+        <div class="row">
+            <div class="always-beautiful section-description wow fadeIn">
+                <h2>
+                    Cante com o TOM
+                </h2>
+                
+                <div class="col-md-6" style="position: relative;">
+                    <img alt="" src="img/coracao.jpg">
+                </div>
+                
+                <div class="col-md-6" style="position: relative;">
+                    <!-- <div class="col-md-6"> -->
+                        <p style="text-align: center; color:#000;">
+                            <i class="fa fa-music" style="position: relative; left: -40%; color: green"></i>
+                           <br>
+                            "Você traz pra mim, alegria e amor <i class="fa fa-music" style="color: blue;-webkit-transform: rotate(180deg);"></i> <br> 
+                            e eu faço um som que parece um tambor.
+                            <i class="fa fa-music" style="color: red;-webkit-transform: rotate(120deg);"></i> Dum tac, dun tac, é despertador? <br>
+                            dum tac dum tac, não é um tambor!<br>
+                            Dum tac,dun tac sou teu coração <br>
+                             e quando te vejo, vermelho eu fico, de tanta emoção" <i class="fa fa-music" style="color: black; -webkit-transform: rotate(160deg);"></i>.
+                            
+                        </p>
+                    <!-- </div> -->
+                    <audio id="bg_audio" controls="controls">
+                        <source src="#" type="audio/ogg">
+                        <source src="audio/musica.mp3" type="audio/mpeg">
+                        Your browser does not support the audio element.
+                    </audio>
+                </div>
+        </div>
+    </div>
+    <div>
+        <img src="img/foguete.png" style="position: relative;top: 120px; left: 25%">
+    </div>
+</div>
+
+
+
+
+<!-- More features -->
+<div class="about-us-container section-container-2 letras-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 more-features section-description wow fadeIn">
+                <h2 class="letras-footer">
+                    Trio Espuleta
+                </h2>
+            </div>
+
+            <script >
+                function showDiv( idName, value ){
+                    objDiv = document.getElementById( idName );
+                    if( value )
+                        objDiv.style.display = "";
+                    else
+                        objDiv.style.display = "none";
+                }
+            </script>
+
+                <div class="col-md-8">
+                     <a href="javascript:void(0)" onclick="javascript:showDiv( 'd1', true );showDiv( 'd2', false );showDiv( 'd3', false );">
+                        <img title="Passe o cursor para me conhecer" src="img/mirela_transp.png" style="position: relative; width: 200px; left: -20px;"
+                            onMouseOver="this.src='img/Menina2.png'"
+                            onMouseOut="this.src='img/mirela_transp.png'"
+                            onClick=""
+                        />
+                    </a>
+
+                    <a href="javascript:void(0)" onclick="javascript:showDiv( 'd1', false );showDiv( 'd2', true );showDiv( 'd3', false );">
+                        <img title="Passe o cursor para me conhecer" src="img/Menina1.png" style="position: relative; width: 200px"
+                            onMouseOver="this.src='img/Menina1.png'"
+                            onMouseOut="this.src='img/mikaela_transp.png'"
+                            onClick=""
+                        />
+                    </a>
+
+                    <a href="javascript:void(0)" onclick="javascript:showDiv( 'd1', false );showDiv( 'd2', false );showDiv( 'd3', true );">
+                        <img title="Passe o cursor para me conhecer" src="img/luca_transp.png" style="position: relative; width: 200px; left: -40px;"
+                            onMouseOver="this.src='img/Menino.png'"
+                            onMouseOut="this.src='img/luca_transp.png'"
+                            onClick=""
+                        />
+                    </a>
+
+                </div>
+                <div class="col-md-4">
+
+                <div id="d1" style="display: none">
+                    <h4 class="descPers">Mirela</h4>
+                     <p style="text-align: justify;">
+                       Mirela é a mais nova e a mais sapeca! Ela sempre encontra uma saída
+                       engraçada para as situações difíceis e por isso ela acaba sempre divertindo
+                       o trio.
+                    </p>
+                </div>
+
+
+                <div id="d2" style="display: block;">
+                    <h4 class="descPers">Mikaela</h4>
+                     <p style="text-align: justify;">
+                       Mikaela é a mais velha, e por isso é a líder do grupo. Ela se preocupa com
+                       os outros dois, e cuida deles sempre quando estão juntos.
+                    </p>
+                </div>
+
+                <div id="d3" style="display: none">
+                    <h4 class="descPers">Lucca</h4>
+                     <p style="text-align: justify;">
+                        Lucca é o mais corajoso e está sempre à frente dos três em qualquer
+                        situação. Ele é o grande herói das meninas.
+                    </p>
+                </div>
+
+                </div>
+
+        </div>
+    </div>
+    <div>
+        <img src="img/bola.png" style="position: relative;top: 150px; left: -25%">
+    </div>
+</div>
+<!-- App Features -->
+<div class="app-features-container section-container-1">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 app-features section-description wow fadeIn">
+                <h2 class="letras-footer" style="color:#47B6BF;position: relative; left: -40%">
+                    O Projeto
+                </h2>
+                <div class="col-md-7 " style="text-align: justify; color: #737373">
+                            <p>
+                                Ieda Domiciano trabalhou em projetos de saúde pública os quais envolviam o
+                                público infantil. Buscando facilitar a comunicação e a educação em saúde,
+                                resolveu criar um canal no qual pudesse ter contado direto com seu público
+                                alvo: as crianças.
+                            </p>
+                            <p>
+                                Por intermédio da série: “As Aventuras do Trio Espuleta”, ela mostra de forma
+                                lúdica e divertida o funcionamento do corpo humano. Transforma o que parece
+                                difícil em uma linguagem fácil e acessível.
+                            </p>
+                            <p>
+                                Por esse canal, as crianças terão oportunidade de aprender o que é “normal” e
+                                saudável no corpo humano para depois entenderem o “anormal”, que são as
+                                doenças.
+                            </p>
+                            <p>
+                                Também haverá oportunidade de dialogar sobre assuntos como bullying,
+                                prevenção do alcoolismo, tabagismo e outros assuntos de saúde/doença que
+                                pode vir a afligir os menores.
+                            </p>
+
+
+                </div>
+                <div class="col-md-5" style="position: relative;top: 20px">
+                    <img alt="" src="img/projeto.jpg"/>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <img src="img/lupa.png" style="position:relative;top: 140px">
+
+    </div>
+</div>
+
+
+
+<!-- How it works -->
+<div class="how-it-works-container section-container" id="how-it-works">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 how-it-works section-description wow fadeIn">
+                <h2>
+                    A Idealizadora
+                </h2>
+                <div class="col-md-4">
+                    <img alt="" src="img/idealizadora.jpg">
+
+                </div>
+                <div class="col-md-8">
+                    <p style="text-align: justify; color: #000">
+                        Formada em Medicina, a Dra. Ieda Domiciano se especializou em Medicina de
+
+                        Família e Comunidade. Em 2012 concluiu o mestrado em ciências na
+
+                        Faculdade de Saúde Pública da USP. Os trabalhos desenvolvidos junto às
+
+                        comunidades priorizavam princípios chaves da Promoção da Saúde tais como,
+
+                        participação social e empoderamento.
+
+                        Ieda também participou do livro “Articulando redes sociais” (2010), escrevendo
+
+                        o capítulo &quot;Educação e Promoção da Saúde: Estratégia para a redução da
+
+                        vulnerabilidade de jovens e adolescentes&quot;.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <img src="img/lapis.png" style="position: relative;top:65px; left:20% ">
+    </div>
+</div>
+
+<div class="blog-container section-container-4">
+    <div class="container letras-footer">
+        <div class="row">
+            <div class="col-sm-12 always-beautiful section-description wow fadeIn">
+                <h2 class="letras-footer">
+                    Blog
+                </h2>
+                <div class="col-md-4" style="position: relative;">
+                    <img alt="" src="img/menino.jpg">
+                </div>
+                <div class="col-md-4" style="position: relative;">
+                    <img alt="" src="img/balao.jpg">
+                </div>
+                <div class="col-md-4" style="position: relative">
+                    <img alt="" src="img/menina.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="contato-container section-container-2">
+    <div class="container">
+        <div class="row letras-footer">
+            <div class="col-sm-12 app-features section-description wow fadeIn ">
+                <h2 class="letras-footer">
+                    Contato
+                </h2>
+                <div class="col-md-12">
+                    <div align="left" style="left: 38%;position: relative;">
+                        <i class="fa fa-envelope fa-3x pb"></i> trioespuleta@gmail.com <br>
+                        <i class="fa fa-phone  fa-3x pb"></i> 19 99806-0711
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <a class="scroll-link" href="#top-content">
+            <img src="img/seta.png" class="img_180" title="Subir ao Topo" />
+        </a>
+    </div>
+</div>
+
+<!-- Footer -->
+<footer class="section-container-5 letras-footer" style="position: relative; bottom: 0px;">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <img alt="" src="img/rodape.jpg">
+                    <a href="">
+                        O Projeto
+                    </a>
+                    |
+                    <a href="">
+                        A Idealizadora
+                    </a>
+                    |
+                    <a href="">
+                        Trio Espuleta
+                    </a>
+                    |
+                    <a href="">
+                        Loja
+                    </a>
+                    |
+                    <a href="">
+                        Divirta-se
+                    </a>
+                    |
+                    <a href="">
+                        Blog
+                    </a>
+                    |
+                    <a href="">
+                        Contato
+                    </a>
+            </div>
+        </div>
+    </div>
+</footer>
+<div class="fuid-container" style="background-color: #009180">
+    <div class="row">
+        <div class="col-md-12">
+            <p style="text-align: justify; color: rgb(255, 161, 0); font-size: 13px; position:relative; left: 30%">
+                Realização: Projet'Art | Design: Agência Zoom - Unasp-EC | Desenvolvimento: <a target="_blank" href="http://www.softlike.com.br" style="color: #fff;">Softlike</a>
+            </p>
+        </div>
+    </div>
+</div>
+
+<?php include_once 'parts/querys.php'; ?>
